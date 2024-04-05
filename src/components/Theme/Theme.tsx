598feb0c41fbd56fa7896de7c8d2d6f6584ce253
Tommy Hoang <72463655+tommyyh@users.noreply.github.com>
2024-04-05 10:49:09 +0000

    Theme system preferred fix: 1.0

--- src/components/Theme/Theme.tsx
+++ src/components/Theme/Theme.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import style from './theme.module.scss';
-import Moon from '@/public/nav/moon.svg';
-import Sun from '@/public/nav/sun.svg';
+import React, { useEffect, useState } from "react";
+import style from "./theme.module.scss";
+import Moon from "@/public/nav/moon.svg";
+import Sun from "@/public/nav/sun.svg";
 
 type PropsType = {
   l: string;
@@ -13,41 +13,42 @@ const Theme = ({ l }: PropsType) => {
   const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    const preferredLoaded = localStorage.getItem('preferredLoaded');
+    const preferredLoaded = localStorage.getItem("preferredLoaded");
 
     // Check if user visits for the first time -> select system preferred theme
-    if (preferredLoaded === 'true') {
+    if (preferredLoaded === "true") {
       // User already visited before
-      const theme = localStorage.getItem('theme') || 'dark';
+      const theme = localStorage.getItem("theme") || "dark";
 
-      document.body.setAttribute('data-theme', theme);
+      document.body.setAttribute("data-theme", theme);
 
-      setDarkMode(theme === 'dark' ? true : false);
+      setDarkMode(theme === "dark" ? true : false);
     } else {
       // User visits for the first time -> Set to know
-      const userPreferred = window.matchMedia('(prefers-color-scheme: dark)');
-      const preferredTheme = userPreferred ? 'dark' : 'light';
+      const userPreferred = window.matchMedia("(prefers-color-scheme: dark)");
+      const preferredTheme = userPreferred ? "dark" : "light";
 
-      document.body.setAttribute('data-theme', preferredTheme);
-      localStorage.setItem('preferredLoaded', 'true');
+      document.body.setAttribute("data-theme", preferredTheme);
+      localStorage.setItem("preferredLoaded", "true");
+      localStorage.setItem("theme", preferredTheme);
 
-      setDarkMode(preferredTheme === 'dark' ? true : false);
+      setDarkMode(preferredTheme === "dark" ? true : false);
     }
   }, []);
 
   const setTheme = () => {
     // Save changes
-    const theme = darkMode ? 'light' : 'dark';
+    const theme = darkMode ? "light" : "dark";
 
-    document.body.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme);
+    document.body.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
 
     setDarkMode(!darkMode);
   };
 
   return (
     <button
-      className={`${style.themeToggle} ${darkMode ? style.darkActive : ''}`}
+      className={`${style.themeToggle} ${darkMode ? style.darkActive : ""}`}
       onClick={setTheme}
       aria-label={l}
     >
