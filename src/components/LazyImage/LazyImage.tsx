b971c2aca41becf702ea4af5ced3a8a269adac2a
Tommy Hoang <72463655+tommyyh@users.noreply.github.com>
2024-04-11 07:33:03 +0000

    Image animation fix

--- src/components/LazyImage/LazyImage.tsx
+++ src/components/LazyImage/LazyImage.tsx
@@ -1,18 +1,18 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Image from 'next/image';
-import style from './lazyImage.module.scss';
-import { motion } from 'framer-motion';
-import generalJson from '@/data/general.json';
+import React from "react";
+import Image from "next/image";
+import style from "./lazyImage.module.scss";
+import { motion } from "framer-motion";
+import generalJson from "@/data/general.json";
 
 const LazyImage = ({ customClass, rawImg = false, ...props }: any) => {
   return !rawImg ? (
     <div className={customClass}>
       <motion.div
-        initial={{ height: '0.18%' }}
-        animate={{ height: '100%' }}
-        transition={{ duration: 1.2, ease: generalJson.imageAnimation }}
+        initial={{ height: "0.18%" }}
+        animate={{ height: "100%" }}
+        transition={{ duration: 0.925, ease: generalJson.imageAnimation }}
         className={style.imgWrapper}
       >
         <Image
